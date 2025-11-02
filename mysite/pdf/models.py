593fdfe9d67aa290df49e9f2a08f644455d06cf8
Uddin Nahim <nahim.211902019@gmail.com>
2025-11-02 19:06:46 +0600

    add proper .gitignore

---
+++ mysite/pdf/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200)
+    summary = models.CharField(max_length=2000)
+    degree = models.CharField(max_length=200)
+    school = models.CharField(max_length=200)
+    University = models.CharField(max_length=200)
+    previous_work = models.CharField(max_length=200)
+    skills = models.CharField(max_length=200)
+
+
