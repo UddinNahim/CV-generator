8486b1d3eedd3a9641c1b0f2bfa8400bface29a0
Uddin Nahim <nahim.211902019@gmail.com>
2025-11-03 00:02:45 +0600

    implement POST method

--- mysite/pdf/models.py
+++ mysite/pdf/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 
 class Profile(models.Model):
+    def __str__(self):
+        return self.name
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     phone = models.CharField(max_length=200)
--- mysite/pdf/views.py
+++ mysite/pdf/views.py
@@ -3,5 +3,18 @@ from .models import Profile
 # Create your views here.
 
 def accept(request):
+    if request.method == "POST":
+        name = request.POST.get("name","")
+        email = request.POST.get("email","")
+        phone = request.POST.get("phone","")
+        summary = request.POST.get("summary","")
+        degree = request.POST.get("degree","")
+        school = request.POST.get("school","")
+        University = request.POST.get("University","")
+        previous_work = request.POST.get("previous_work","")
+        skills = request.POST.get("skills","")
+
+        profile = Profile(name=name,email=email,phone=phone,summary=summary,degree=degree,school=school,University=University,previous_work=previous_work,skills=skills)
+        profile.save()
 
     return render(request,'pdf/accept.html')
