de1765e69db776520da98caf72cc49c6b5fecdcf
Uddin Nahim <nahim.211902019@gmail.com>
2025-11-03 00:22:09 +0600

    building resume templates

--- mysite/pdf/views.py
+++ mysite/pdf/views.py
@@ -14,7 +14,12 @@ def accept(request):
         previous_work = request.POST.get("previous_work","")
         skills = request.POST.get("skills","")
 
-        profile = Profile(name=name,email=email,phone=phone,summary=summary,degree=degree,school=school,University=University,previous_work=previous_work,skills=skills)
+        profile = Profile(name=name,email=email,phone=phone,summary=summary,degree=degree,school=school,university=University,previous_work=previous_work,skills=skills)
         profile.save()
 
     return render(request,'pdf/accept.html')
+
+def resume(request,id):
+    user_profile = Profile.objects.get(pk=id)
+    return render(request,'pdf/resume.html',{'user_profile':user_profile,})
+
